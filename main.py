f8a6142f43def649c11d2af69dde2f43e0cd6cf2
Keri Sartain <kittenwarrior@gmail.com>
2017-01-25 21:58:05 -0600

    wrapped encrypted message in textarea inside form, added submit button

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import webapp2
 import caesar
 
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         message = 'Hellooooo World!'
         encrypted_message = caesar.encrypt(message, 13)
-        self.response.write(encrypted_message)
+        textarea = "<textarea>" + encrypted_message + "</textarea>"
+        submit = "<input type='submit'/>"
+        form = "<form>" + textarea + "<br>" + submit + "</form"
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
